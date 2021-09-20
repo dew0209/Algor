3077f5838c979d9e8caa6b91753724fef2c3a70a
dew0209 <ly020922@aliyun.com>
2021-09-20 19:58:46 +0800

    测试

--- atcoder/219/A.cpp
+++ atcoder/219/A.cpp
@@ -1,1 +1,11 @@
-﻿#include <iostream>#include <cstring>#include <cmath>using namespace std;int main(){    cout << "测试" << endl;    return 0;}
+﻿#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout << "测试" << endl;
+    cout << "啦啦啦" << endl;
+
+    return 0;
+}

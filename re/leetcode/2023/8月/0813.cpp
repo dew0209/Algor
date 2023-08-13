ed96214495768fd77c1fbaea84c406f239845cc6
dew0209 <ly020922@aliyun.com>
2023-08-13 10:26:35 +0800

    [计划][leetcode][0813][每日一题][合并两个有序数组]

---
+++ re/leetcode/2023/8月/0813.cpp
@@ -0,0 +1,41 @@
+题目链接：https://leetcode.cn/problems/merge-sorted-array/description/
+
+方法一：暴力
+
+nums1:[1,2,3,0,0,0]  3
+nums2:[2,5,6] 3
+
+开额外空间
+vector<int> a;
+
+int i = 0,j = 0;
+if(nums1[i] < nums2[j])a.push_back(nums1[i++]);
+else a.push_back(nums2[j++]);
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> a;
+        int i = 0,j = 0;
+        for(i = 0,j = 0;i < m && j < n;){
+            if(nums1[i] > nums2[j])a.push_back(nums2[j++]);
+            else a.push_back(nums1[i++]);
+        }
+        //这里需要处理没有比较的数据
+        while(i < m)a.push_back(nums1[i++]);
+        while(j < n)a.push_back(nums2[j++]);
+        for(int i = 0;i < n + m;i++){
+            nums1[i] = a[i];
+        }
+    }
+};
+
+
+方法二：
+nums1:[1,2,3,0,0,0]  3
+nums2:[2,5,6] 3
+    m - 1 id
+1 2 3 0 0 0
+    n - 1
+2 5 6

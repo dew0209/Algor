050655b62f8b18663a01040433a0fdc8e5fbcb74
Guitarist <89758002+dew0209@users.noreply.github.com>
2025-04-14 17:09:06 +0800

    [计划][leetcode][0414][每日一题]

---
+++ re/leetcode/2025/04/14.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.cn/problems/count-good-triplets/description/?envType=daily-question&envId=2025-04-14
+
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int n = arr.size();
+            int res = 0;
+            for(int i = 0;i < n;i++){
+                for(int j = i + 1;j < n;j++){
+                    for(int k = j + 1;k < n;k++){
+                        bool fa = abs(arr[i] - arr[j]) <= a;
+                        bool fb = abs(arr[j] - arr[k]) <= b;
+                        bool fc = abs(arr[i] - arr[k]) <= c;
+                        if(fa && fb && fc){
+                            res++;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    };

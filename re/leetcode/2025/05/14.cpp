458c704654d51523cd0a5ca6a1afd60c1d1a47fb
Guitarist <89758002+dew0209@users.noreply.github.com>
2025-05-14 13:15:29 +0800

    [计划][leetcode][0514][每日一题]

---
+++ re/leetcode/2025/05/14.cpp
@@ -0,0 +1,70 @@
+//https://leetcode.cn/problems/total-characters-in-string-after-transformations-ii/description/?envType=daily-question&envId=2025-05-14
+
+
+const int MOD = 1e9 + 7;
+class Solution {
+public:
+
+    vector<vector<int>> mul(vector<vector<int>> a,vector<vector<int>> b){
+        vector<vector<int>> res(26,vector<int>(26,0));;
+        
+        for(int i = 0;i < 26;i++){
+            for(int k = 0;k < 26;k++){
+                if(a[i][k] == 0)continue;
+                for(int j = 0;j < 26;j++){
+                    res[i][j] = (res[i][j] + (long long)a[i][k] * b[k][j]) % MOD;
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<vector<int>> mul(vector<vector<int>> a,int b){
+        vector<vector<int>> res(26,vector<int>(26,0));;
+        for(int i = 0;i < 26;i++){
+            res[i][i] = 1;
+        }
+        while(b){
+            if(b & 1){
+                res = mul(res,a);
+            }
+            a = mul(a,a);
+            b >>= 1;
+        }
+        return res;
+    }
+
+    int lengthAfterTransformations(string s, int t, vector<int>& nums) {
+        //矩阵
+        vector<vector<int>> m(26,vector<int>(26,0));
+
+        //转换规则基础矩阵构造
+        for(int i = 0;i < 26;i++){
+            for(int j = i + 1;j <= i + nums[i];j++){
+                m[i][j % 26] = 1;
+            }
+        }
+
+        for(int i = 0;i < 26;i++){
+            for(int j = 0;j < 26;j++){
+                cout << m[i][j] << ' ';
+            }
+            cout << endl;
+        }
+
+        auto mt = mul(m,t);
+
+        vector<int> cnt(26,0);
+
+        for(int i = 0;i < s.size();i++){
+            cnt[s[i] - 'a']++;
+        }
+
+        long long res = 0;
+
+        for(int i = 0;i < 26;i++){
+            res = res + reduce(mt[i].begin(), mt[i].end(), 0LL) * cnt[i] % MOD;
+        }
+        return res % MOD;
+    }
+};

ed097764f76d1ff2efd49683d96d46348bfce5cb
jsyx <ly020922@aliyun.com>
2026-05-14 16:30:10 +0800

    [leetcode每日一题]

---
+++ leetcode/每日一题/2026/05/12.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.cn/problems/minimum-initial-energy-to-finish-tasks/description/?envType=daily-question&envId=2026-05-12
+
+
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(),tasks.end(),[](auto a,auto b){return (a[1] - a[0]) < (b[1] - b[0]);});
+        int ans = 0;
+        for(auto& task : tasks){
+            int a = task[0],minv = task[1];
+            ans = max(ans + a,minv);
+        }
+        return ans;
+    }
+};
